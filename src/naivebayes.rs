61a215953da020acb130ef9570c0c41cf540369f
aduric <adnan.duric@d2l.com>
2017-08-21 18:57:03 -0400

    Fixes some syntax issues

--- src/naivebayes.rs
+++ src/naivebayes.rs
@@ -1,8 +1,8 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{HashMap, Hasher};
 
-pub type Feature<&str, T> = (&str, T);
-pub type Label<&str> = &str;
+pub type Feature<T> = (&str, T);
+pub type Label = &str;
 
 pub struct NaiveBayes {
     training_docs: Vec<Document>,
@@ -10,24 +10,24 @@ pub struct NaiveBayes {
 }
 
 pub struct Model {
-    features: Vec<Feature>,
+    features: Vec<Feature<T>>,
     labels: Vec<Label>,
-    label_likelihoods: HashMap<Label, HashMap<Feature, f64>>
+    label_likelihoods: HashMap<Label, HashMap<Feature<T>, f64>>
 }
 
 impl Model {
-    pub fn get_likelihoods() -> HashMap<Label, HashMap<Feature, f64>> {
+    pub fn get_likelihoods() -> HashMap<Label, HashMap<Feature<T>, f64>> {
         self.label_likelihoods
     }
 }
 
 impl NaiveBayes {
-    pub fn train(featureset: Vec<(Vec<Feature>, Label)> -> Self {
+    pub fn train(featureset: Vec<(Vec<Feature<T>>, Label)>) -> Self {
         let model = Model::new();
 
         let label_totals: HashMap<&str, u32> = HashMap::new();
 
-        let feature_counts = HashMap<Label, HashMap<Feature, u32>::new();
+        let feature_counts: HashMap<Label, HashMap<Feature<T>, u32>> = HashMap::new();
         for f in featureset.iter() {
             let label = f.1;
             let label_totals_incr = label_totals.entry(*label).or_insert(0);
@@ -47,7 +47,7 @@ impl NaiveBayes {
 
         model
     }
-    pub fn test(featureset: Vec<(Vec<Feature>, Label)> -> Self {
+    pub fn test(featureset: Vec<(Vec<Feature<T>>, Label)>) -> Self {
     }
 }
 
@@ -60,18 +60,18 @@ mod tests {
     fn train_test() {
         let mut nb = NaiveBayes::new();
 
-        let featureset = vec![
-            (Feature::new("last-letter", 'a'), Label::new("female")),
-            ({ "last-letter": 'e'}, "female"),
-            ({ "last-letter": 'k'}, "male"),
-            ({ "last-letter": 'p'), "male"),
-        ]
+        let featureset: Vec<(Vec<Feature<&str>>, Label)> = vec![
+            (vec![("last-letter", 'a')], "female"),
+            (vec![( "last-letter", 'e')], "female"),
+            (vec![( "last-letter", 'k')], "male"),
+            (vec![( "last-letter", 'p')], "male"),
+        ];
 
         let test_label_likelihoods = HashMap::new();
         test_label_likelihoods.insert("male", ({ "last-letter": 'k'}, 0.9));
 
         let model = nb.train(&featureset);
 
-        assert!((&model.get_label_likelihoods() == test_label_likelihoods);
+        assert!(&model.get_label_likelihoods() == test_label_likelihoods);
     }
 }
